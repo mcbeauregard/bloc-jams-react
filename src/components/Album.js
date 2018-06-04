e4b6be85d31e2013b000d6cb3b4438c53005c09c
mcbeauregard <mcbeauregard@gmail.com>
2018-06-03 22:13:45 -0700

    First commit - created PlayerBar file

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 import PlayerBar from './PlayerBar';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
     constructor(props) {
