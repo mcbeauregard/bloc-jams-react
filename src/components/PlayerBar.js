a67be1bbf654e6af12ee73c51f2f42eeab2fc412
mcbeauregard <mcbeauregard@gmail.com>
2018-06-05 11:37:16 -0700

    Added the onChange Event Listener to PlayerBar - checkpoint complete

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -15,7 +15,7 @@ class PlayerBar extends Component {
             <span className="ion-skip-forward"></span>
           </button>
         </section>
-        <section id="time-control"> 
+        <section id="time-control">
         <div className="current-time">{this.props.currentTime}</div>
         <input
           type="range"
@@ -24,6 +24,7 @@ class PlayerBar extends Component {
           max="1"
           min="0"
           step="0.01"
+          onChange={this.props.handleTimeChange}
         />
         <div className="total-time">{this.props.duration}</div>
         </section>
