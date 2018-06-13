c68dcbba4ffe6d326c603b681e1ae5658a30a1f5
mcbeauregard <mcbeauregard@gmail.com>
2018-06-13 11:36:04 -0700

    landing page hero section almost complete

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,6 +6,7 @@ const Landing = () => (
   <section className="landing">
     <h1 className="hero-title">Turn the music up!</h1>
     <section className="selling-points">
+      <div className="selling-points-left">
       <div className="point">
        <h2 className="point-title">Choose your music</h2>
        <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
@@ -18,11 +19,11 @@ const Landing = () => (
        <h2 className="point-title">Mobile enabled</h2>
        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
       </div>
-      <div className="album-covers">
+      </div>
+      <div className="selling-points-right">
         <img className="album-covers-01" src='./../assets/images/album_covers/01.jpg'/>
         <img className="album-covers-02" src='./../assets/images/album_covers/02.jpg'/>
         <img className="album-covers-03" src='./../assets/images/album_covers/03.jpg'/>
-        <img className="album-covers-04" src='./../assets/images/album_covers/04.jpg'/>
       </div>
     </section>
   </section>
