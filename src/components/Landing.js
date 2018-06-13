bf84b8fbebd34c7fa5f2920fce3be6994bb97b4c
mcbeauregard <mcbeauregard@gmail.com>
2018-06-13 08:37:43 -0700

    add album covers to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -18,6 +18,12 @@ const Landing = () => (
        <h2 className="point-title">Mobile enabled</h2>
        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
       </div>
+      <div className="album-covers">
+        <img className="album-covers-01" src='./../assets/images/album_covers/01.jpg'/>
+        <img className="album-covers-02" src='./../assets/images/album_covers/02.jpg'/>
+        <img className="album-covers-03" src='./../assets/images/album_covers/03.jpg'/>
+        <img className="album-covers-04" src='./../assets/images/album_covers/04.jpg'/>
+      </div>
     </section>
   </section>
 );
