8a57e75790951d36654e11580e6b5368e0632a22
mcbeauregard <mcbeauregard@gmail.com>
2018-06-13 13:55:34 -0700

    Library page: configure albums:

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import styles from './Library.css';
 
 class Library extends Component {
     constructor(props) {
@@ -15,9 +16,11 @@ class Library extends Component {
           this.state.albums.map( (album, index) =>
             <Link to={`/album/${album.slug}`} key={index}>
             <img src={album.albumCover} alt={album.title} />
-            <div>{album.title}</div>
-            <div>{album.artist}</div>
-            <div>{album.songs.length} songs</div>
+            <section className='library-text'>
+              <div>{album.title}</div>
+              <div>{album.artist}</div>
+              <div>{album.songs.length} songs</div>
+            </section>
             </Link>
           )
         }
